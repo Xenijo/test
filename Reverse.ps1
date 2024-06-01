a6c5d26fe212881ee191c8b66099628e84067a71
Xenijo <80117820+Xenijo@users.noreply.github.com>
2024-06-01 15:35:52 +0200

    Update Reverse.ps1

--- Reverse.ps1
+++ Reverse.ps1
@@ -20,8 +20,32 @@ Set-MpPreference -SubmitSamplesConsent AlwaysPrompt
 # Restore file attributes
 attrib -r C:\Windows\System32\drivers\etc\hosts
 
-# Remove bound.exe registry entry
-Remove-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\InventoryApplicationFile\bound.exe|18399b81ecce1834\Version" -Force
+# Remove bound.exe registry entries
+Remove-Item -Path "HKCU:\Software\Classes\Local Settings\MuiCache\4d\52C64B7E\@%SystemRoot%\system32\mlang.dll,-4386" -Force
+Remove-Item -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\AmiHiveOwnerCorrect" -Force
+Remove-Item -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\AmiHivePermissionsCorrect" -Force
+Remove-Item -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\Windows Error Reporting\Debug\ExceptionRecord" -Force
+
+# Remove InventoryApplicationFile\bound.exe registry entries
+$inventoryPath = "REGISTRY::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\InventoryApplicationFile"
+Remove-Item -Path "$inventoryPath\bound.exe|18399b81ecce1834\BinFileVersion" -Force
+Remove-Item -Path "$inventoryPath\bound.exe|18399b81ecce1834\BinProductVersion" -Force
+Remove-Item -Path "$inventoryPath\bound.exe|18399b81ecce1834\BinaryType" -Force
+Remove-Item -Path "$inventoryPath\bound.exe|18399b81ecce1834\FileId" -Force
+Remove-Item -Path "$inventoryPath\bound.exe|18399b81ecce1834\IsOsComponent" -Force
+Remove-Item -Path "$inventoryPath\bound.exe|18399b81ecce1834\IsPeFile" -Force
+Remove-Item -Path "$inventoryPath\bound.exe|18399b81ecce1834\Language" -Force
+Remove-Item -Path "$inventoryPath\bound.exe|18399b81ecce1834\LinkDate" -Force
+Remove-Item -Path "$inventoryPath\bound.exe|18399b81ecce1834\LongPathHash" -Force
+Remove-Item -Path "$inventoryPath\bound.exe|18399b81ecce1834\LowerCaseLongPath" -Force
+Remove-Item -Path "$inventoryPath\bound.exe|18399b81ecce1834\Name" -Force
+Remove-Item -Path "$inventoryPath\bound.exe|18399b81ecce1834\ProductName" -Force
+Remove-Item -Path "$inventoryPath\bound.exe|18399b81ecce1834\ProductVersion" -Force
+Remove-Item -Path "$inventoryPath\bound.exe|18399b81ecce1834\ProgramId" -Force
+Remove-Item -Path "$inventoryPath\bound.exe|18399b81ecce1834\Publisher" -Force
+Remove-Item -Path "$inventoryPath\bound.exe|18399b81ecce1834\Size" -Force
+Remove-Item -Path "$inventoryPath\bound.exe|18399b81ecce1834\Usn" -Force
+Remove-Item -Path "$inventoryPath\bound.exe|18399b81ecce1834\Version" -Force
 
 # Additional cleanup actions if needed
 # (No reverse actions needed for queries or commands that didn't change the system state)

0276394db6c02a12019ba9bd0dbad18e0f99cee0
Xenijo <80117820+Xenijo@users.noreply.github.com>
2024-06-01 19:49:05 +0200

    Update Reverse.bat

--- Reverse.bat
+++ Reverse.bat
@@ -1,25 +1,41 @@
 @echo off
+SETLOCAL
 
-rem Remove exclusion paths from Windows Defender
-echo Removing exclusion paths from Windows Defender...
-"%ProgramFiles%\Windows Defender\MpCmdRun.exe" -RemoveExclusion "%USERPROFILE%\AppData\Local\Temp\bound.exe"
-"%ProgramFiles%\Windows Defender\MpCmdRun.exe" -RemoveExclusion "%USERPROFILE%\Desktop\software.exe"
+:: Path to the hosts file
+set HOSTS_FILE=%SystemRoot%\System32\drivers\etc\hosts
 
-rem Re-enable Windows Defender features
-echo Re-enabling Windows Defender features...
-powershell -Command "Set-MpPreference -DisableIntrusionPreventionSystem $false"
-powershell -Command "Set-MpPreference -DisableIOAVProtection $false"
-powershell -Command "Set-MpPreference -DisableRealtimeMonitoring $false"
-powershell -Command "Set-MpPreference -DisableScriptScanning $false"
-powershell -Command "Set-MpPreference -EnableControlledFolderAccess Enabled"
-powershell -Command "Set-MpPreference -EnableNetworkProtection Block"
-powershell -Command "Set-MpPreference -MAPSReporting Advanced"
-powershell -Command "Set-MpPreference -SubmitSamplesConsent AlwaysPrompt"
+:: Backup the current hosts file
+copy "%HOSTS_FILE%" "%HOSTS_FILE%.bak"
 
-rem Restore file attributes
-echo Restoring file attributes...
-attrib -r "C:\Windows\System32\drivers\etc\hosts"
+:: Set the new content for the hosts file
+set "HOSTS_CONTENT=# Copyright (c) 1993-2009 Microsoft Corp.%newline%
+#%newline%
+# This is a sample HOSTS file used by Microsoft TCP/IP for Windows.%newline%
+#%newline%
+# This file contains the mappings of IP addresses to host names. Each%newline%
+# entry should be kept on an individual line. The IP address should%newline%
+# be placed in the first column followed by the corresponding host name.%newline%
+# The IP address and the host name should be separated by at least one%newline%
+# space.%newline%
+#%newline%
+# Additionally, comments (such as these) may be inserted on individual%newline%
+# lines or following the machine name denoted by a '#' symbol.%newline%
+#%newline%
+# For example:%newline%
+#%newline%
+#      102.54.94.97     rhino.acme.com          # source server%newline%
+#       38.25.63.10     x.acme.com              # x client host%newline%
+#%newline%
+# localhost name resolution is handled within DNS itself.%newline%
+#    127.0.0.1       localhost%newline%
+#    ::1             localhost"
 
-rem Print completion message
-echo Reversal of changes is complete.
+:: Write the new content to the hosts file
+(
+    echo %HOSTS_CONTENT%
+) > "%HOSTS_FILE%"
+
+echo Hosts file has been updated successfully.
+
+ENDLOCAL
 pause
